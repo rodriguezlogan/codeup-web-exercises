1912d18d8217fbc169d02e7ec31d4dff6aa35420
Logan Rodriguez <Rodriguezlogan0428@gmail.com>
2023-03-13 10:59:11 -0500

    updating

--- js/javascript-with-functions.js
+++ js/javascript-with-functions.js
@@ -2,3 +2,7 @@
 function multiples(a,b,c) {
     alert(a*b*c);
 }
+var first = prompt("give me the first number");
+var second = prompt("give me the second number");
+var third = prompt("give me the third number");
+multiples(first,second,third);
