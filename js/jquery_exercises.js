eaf5ab32f9b1b0237a21593535b6b618e36d1384
Logan Rodriguez <Rodriguezlogan0428@gmail.com>
2023-04-19 10:02:24 -0500

    practicing jquery

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,7 +1,16 @@
 "use strict";
 
-$(function() {
+// $(function() {
+//
+//     alert("the DOM has finished loading");
+//
+// });
 
-    alert("the DOM has finished loading");
+// $('#first').css('background-color', 'yellow');
 
-});
+// $('.codeup').css('border', 'solid 1px red');
+
+$('li').css('font-size', '20px');
+$('h1, p, li').css('background-color', 'yellow');
+var contents = $('h1').html();
+alert(contents);
