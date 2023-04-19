bd6d5decee2f1a6d9d0e217ca6a7c973345051c8
Logan Rodriguez <Rodriguezlogan0428@gmail.com>
2023-04-19 13:49:11 -0500

    messing with things

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -10,7 +10,23 @@
 
 // $('.codeup').css('border', 'solid 1px red');
 
-$('li').css('font-size', '20px');
-$('h1, p, li').css('background-color', 'yellow');
-var contents = $('h1').html();
-alert(contents);
+// $('li').css('font-size', '20px');
+// $('h1, p, li').css('background-color', 'yellow');
+// var contents = $('h1').html();
+// alert(contents);
+$('h1').click(function() {
+    $('h1').css('background-color', 'green');
+});
+
+$('p').dblclick(function() {
+    $('p').css('font-size', '18px');
+});
+
+$('li').hover(
+    function() {
+        $(this).css('color', 'red');
+    },
+    function() {
+        $(this).css('color', 'black');
+    }
+);
